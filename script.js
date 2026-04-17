0bd4579c697999b2933c1a2cd1212f8c91944f27
pattern-screamer <schuurmanlukas@gmail.com>
2026-04-17 17:35:54 +0200

    Finish application

--- script.js
+++ script.js
@@ -108,6 +108,7 @@ const Cell = () => {
 
 const Player = (id, token) => {
   let name = `Player ${id}`;
+  let score = 0;
   let color;
 
   const getName = () => {
@@ -118,6 +119,18 @@ const Player = (id, token) => {
     name = newName;
   }
 
+  const getScore = () => {
+    return score;
+  }
+
+  const incrementScore = () => {
+    score++;
+  }
+
+  const resetScore = () => {
+    score = 0;
+  }
+
   const getColor = () => {
     return color;
   }
@@ -126,11 +139,21 @@ const Player = (id, token) => {
     color = hex;
   }
 
-  return { id, token, getName, setName, getColor, setColor };
+  return {
+    id,
+    token,
+    getName,
+    setName,
+    getScore,
+    incrementScore,
+    resetScore,
+    getColor,
+    setColor
+  };
 };
 
 const gameController = (() => {
-  const player = [
+  const players = [
     Player(1, "X"),
     Player(2, "O")
   ];
@@ -138,19 +161,27 @@ const gameController = (() => {
   let activePlayer;
 
   const switchPlayer = () => {
-    activePlayer = activePlayer === player[0] ? player[1] : player[0];
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  }
+
+  const getPlayerName = (playerId) => {
+    return players[playerId - 1].getName();
   }
 
   const setPlayerName = (playerId, newName) => {
-    player[playerId - 1].setName(newName);
+    players[playerId - 1].setName(newName);
+  }
+
+  const getPlayerScore = (playerId) => {
+    return players[playerId - 1].getScore();
   }
 
   const getPlayerColor = (playerId) => {
-    return player[playerId - 1].getColor();
+    return players[playerId - 1].getColor();
   }
 
   const setPlayerColor = (playerId, newColor) => {
-    player[playerId - 1].setColor(newColor);
+    players[playerId - 1].setColor(newColor);
   }
 
   /**
@@ -171,6 +202,11 @@ const gameController = (() => {
       if (gameBoard.checkWin()) {
         gameState = 0;
         status = 'win';
+        for (const player in players) {
+          if (players[player].token === gameBoard.checkWin()) {
+            players[player].incrementScore();
+          }
+        }
       } else if (gameBoard.checkTie()) {
         gameState = 0;
         status = 'tie';
@@ -207,23 +243,37 @@ const gameController = (() => {
   const buildGame = () => {
     gameBoard.build();
     gameState = 1;
-    activePlayer = player[0];
+    activePlayer = players[0];
     status = 'turn';
   }
 
   const resetGame = () => {
+    for (const player in players) {
+      players[player].resetScore();
+    }
     gameBoard.build();
     gameState = 0;
     status = null;
   }
 
-  return { playRound, getBoard, resetGame, getStatus, setPlayerName, getPlayerColor, setPlayerColor }
+  return {
+    playRound,
+    getBoard,
+    resetGame,
+    getStatus,
+    getPlayerName,
+    setPlayerName,
+    getPlayerScore,
+    getPlayerColor,
+    setPlayerColor
+  }
 })();
 
 const displayController = (() => {
   const boardDisplay = document.querySelector("div#board");
   const statusDisplay = document.querySelector("div#status");
-  const scoreDisplay = document.querySelector("div#score");
+  const playerOneScore = document.querySelector("p#player-one-score");
+  const playerTwoScore = document.querySelector("p#player-two-score");
 
   const buildDisplay = () => {
     boardDisplay.innerHTML = "";
@@ -241,6 +291,8 @@ const displayController = (() => {
 
     const status = gameController.getStatus();
     statusDisplay.textContent = status;
+    playerOneScore.textContent = gameController.getPlayerScore(1);
+    playerTwoScore.textContent = gameController.getPlayerScore(2);
   }
 
   const updateDisplay = () => {
@@ -252,8 +304,10 @@ const displayController = (() => {
       cell.textContent = board[column][row];
     }
 
-    const status = gameController.getStatus(); 
+    const status = gameController.getStatus();
     statusDisplay.textContent = status;
+    playerOneScore.textContent = gameController.getPlayerScore(1);
+    playerTwoScore.textContent = gameController.getPlayerScore(2);
   }
 
   const clickCell = (event) => {
@@ -271,7 +325,7 @@ const displayController = (() => {
   const flipStart = () => {
     gameStarted = !gameStarted;
     if (gameStarted) {
-       gameController.playRound(0, 0);
+      gameController.playRound(0, 0);
     }
     boardDisplay.classList.toggle('disabled');
   }
@@ -283,14 +337,24 @@ const inputController = (() => {
   const playerOneInput = document.querySelector('input#player-one');
   const playerTwoInput = document.querySelector('input#player-two');
   const startRestart = document.querySelector('button#start-restart');
-  
+  const playerOneName = document.querySelector("p#player-one-name");
+  const playerTwoName = document.querySelector("p#player-two-name");
+
   content = ['Start', 'Restart'];
   const clickStartRestart = () => {
     if (!playerOneInput.disabled && playerOneInput.value) {
       gameController.setPlayerName(1, (playerOneInput.value));
+      playerOneName.textContent = gameController.getPlayerName(1);
+    } else if (!playerOneInput.value) {
+      gameController.setPlayerName(1, `Player 1`);
+      playerOneName.textContent = `Player 1`;
     }
     if (!playerTwoInput.disabled && playerTwoInput.value) {
       gameController.setPlayerName(2, (playerTwoInput.value));
+      playerTwoName.textContent = gameController.getPlayerName(2);
+    } else if (!playerTwoInput.value) {
+      gameController.setPlayerName(2, `Player 2`);
+      playerTwoName.textContent = `Player 2`;
     }
     playerOneInput.disabled = !playerOneInput.disabled
     playerTwoInput.disabled = !playerTwoInput.disabled
